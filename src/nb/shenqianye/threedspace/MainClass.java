108768509e2f864d8eb3c5e590fa1724f93c98c4
王千夜 <51922539+SBshenqianye@users.noreply.github.com>
2021-09-14 22:31:51 +0800

    删除测试用的组件；实现新建窗口、监听键鼠等功能；

--- src/nb/shenqianye/threedspace/MainClass.java
+++ src/nb/shenqianye/threedspace/MainClass.java
@@ -1,89 +1,50 @@
 package nb.shenqianye.threedspace;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeListener;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class MainClass {
     public static void main(String[] args){
         System.out.print("测试\n");
-        MainClass.mainMenuOperation();
+        mainWindow();
     }
-
-    static void mainMenuOperation() {
-        NbWindow mainwindow = new NbWindow();
-        mainwindow.createAndShowGUI();
-       // mainwindow.createAndShowDialog(mainwindow.frame);
-        System.out.print("运行完毕成员方法创造并显示对话框");
+    static void mainWindow() {
+        NBWindow mainWindow = new NBWindow();
+        mainWindow.create();
     }
 }
-
-
-class NbWindow{
-    int id;
-    JFrame frame = new JFrame("一个新的、大的、方形的窗口");
-    NbWindow(){
-        System.out.print("创建了一个虚拟窗口\n");
-    }
-
-    void createAndShowGUI(){
-        JButton btn1 = new JButton("查看讯息1");
-        JButton btn2 = new JButton("查看讯息2");
+/*1.需要一个窗口*/
+class NBWindow{
+    JFrame frame = new JFrame();
+    void create(){
+        this.frame.setLocation(100, 100);
+        this.frame.setSize(650, 400);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setTitle("一个新的、大的、方形的窗口");
-        this.frame.setSize(1080, 700);
-        this.frame.setLocation(0, 0);
+        this.frame.setTitle("3D空间");
         this.frame.setVisible(true);
-        System.out.print("成功创建一个窗口\n");
-
-        btn1.addActionListener(new ActionListener() {
+        this.frame.addKeyListener(new KeyAdapter() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                createAndShowDialog(frame, 1);
+            public void keyTyped(KeyEvent e) {
+                char key = e.getKeyChar();
+                if(key == 'a' || key == 'A') System.out.print("向左移动\n");
+                else if(key == 'd' || key == 'D') System.out.print("向右移动\n");
+                else if(key == 'w' || key == 'W') System.out.print("向上移动\n");
+                else if(key == 's' || key == 'S') System.out.print("向下移动\n");
             }
         });
-        this.frame.add(btn1);
-        btn2.addActionListener(new ActionListener() {
+        this.frame.addMouseListener(new MouseAdapter() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                createAndShowDialog(frame, 2);
+            public void mouseClicked(MouseEvent e) {
+                if( e.getButton() == MouseEvent.BUTTON1) System.out.print("按下鼠标左键\n");
+                else if( e.getButton() == MouseEvent.BUTTON2) System.out.print("按下鼠标中键\n");
+                else if( e.getButton() == MouseEvent.BUTTON3) System.out.print("按下鼠标右键\n");
+            }
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                ;
             }
         });
-        this.frame.add(btn2);
-    }
-    void createAndShowDialog(JFrame inframe, int judge) {
-        JDialog dialog = new JDialog(inframe);
-        JButton btn1 = new JButton("确实");
-        JLabel label1 = new JLabel();
-
-        /*设置属性*/
-        dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-        dialog.setTitle("dialog1");
-        dialog.setSize(300, 140);
-        dialog.setLocation(108, 70);
-
-        System.out.print("成功创建一个对话框\n");
-
-        /*增加组件*/
-
-        btn1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        dialog.add(btn1);
-
-        if(judge == 1) {
-            label1.setText("你是狗");
-        }
-        else if(judge == 2){
-            label1.setText("恭喜您，您不是狗");
-        }
-        dialog.add(label1);
-
-        dialog.setVisible(true);
     }
 }
+/*2.需要能够检测键盘与鼠标的指令*/
+/*3.需要能够绘制图像*/
